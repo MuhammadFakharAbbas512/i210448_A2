a6b99c5b9822484e8cf14230dc8153a3f3f3b7ed
i210448 Muhammad Fakhar Abbas <i210448@nu.edu.pk>
2024-03-20 19:56:23 +0500

    Search Mentor with Results

--- 21i0448/app/src/main/java/com/muhammadfakharabbas/i210448/pg8find.kt
+++ 21i0448/app/src/main/java/com/muhammadfakharabbas/i210448/pg8find.kt
@@ -1,33 +1,81 @@
 package com.muhammadfakharabbas.i210448
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class pg8find : AppCompatActivity() {
+
+    private lateinit var mentorAdapter: MentorAdapter
+    private lateinit var mentorRecyclerView: RecyclerView
+    private lateinit var mentorList: ArrayList<MentorModel>
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var msg: String
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pg8find)
+
+
+//        var auth = FirebaseAuth.getInstance()
+
+        mentorRecyclerView = findViewById(R.id.search_rv)
+
+        mentorList = ArrayList()
+        mentorAdapter = MentorAdapter(this, mentorList)
+        mentorRecyclerView.adapter = mentorAdapter
+        mentorRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        currentUser = FirebaseAuth.getInstance().currentUser!!
+        databaseReference = FirebaseDatabase.getInstance().getReference("mentors")
+        getMentorsList()
+
         var home_btn = findViewById<ImageView>(R.id.home_img)
         var search_btn = findViewById<ImageView>(R.id.search_img)
         var add_btn = findViewById<ImageView>(R.id.add_img)
         var chat_btn = findViewById<ImageView>(R.id.chat_img)
         var profile_btn = findViewById<ImageView>(R.id.profile_img)
 
+        var search = findViewById<ImageView>(R.id.search)
+        var rv = findViewById<RecyclerView>(R.id.search_rv)
+        var searchInput = findViewById<EditText>(R.id.search_edtxt)
+        searchInput.requestFocus()
         var back_btn = findViewById<Button>(R.id.back_btn)
 
-        var mentor1 = findViewById<TextView>(R.id.mentor1)
-        var mentor2 = findViewById<TextView>(R.id.mentor2)
-        var mentor3 = findViewById<TextView>(R.id.mentor3)
+         // var mentor1 = findViewById<TextView>(R.id.mentor1)
+        //var mentor2 = findViewById<TextView>(R.id.mentor2)
+       // var mentor3 = findViewById<TextView>(R.id.mentor3)
         var img1 = findViewById<ImageView>(R.id.img1)
         var img2 = findViewById<ImageView>(R.id.img2)
         var img3 = findViewById<ImageView>(R.id.img3)
         var img11 = findViewById<ImageView>(R.id.img11)
         var img22 = findViewById<ImageView>(R.id.img22)
         var img33 = findViewById<ImageView>(R.id.img33)
+
+        search.setOnClickListener{
+
+            var str = searchInput.text.toString()
+            if (str.isEmpty() || str.length <3){
+                Toast.makeText(this, "Field cannot be empty/Enter more terms", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            search(str)
+        }
 
         back_btn.setOnClickListener{
             val intent = Intent(this, pg7home::class.java)
@@ -55,6 +103,8 @@ class pg8find : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+        /*
         mentor1.setOnClickListener{
             val intent = Intent(this, pg9searchresults::class.java)
             startActivity(intent)
@@ -90,6 +140,63 @@ class pg8find : AppCompatActivity() {
         img33.setOnClickListener{
             val intent = Intent(this, pg9searchresults::class.java)
             startActivity(intent)
-        }
+        }*/
+    }
+
+    fun search(str:String){
+       // var q = FirebaseUtil.allUserDatabaseReference().whereGreaterThanOrEqualTo("username", str)
+        databaseReference = FirebaseDatabase.getInstance().getReference("mentors")
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@pg8find, "Database error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mentorList.clear()
+
+                for (dataSnapshot in snapshot.children) {
+                    val mentor = dataSnapshot.getValue(MentorModel::class.java)
+                    mentor?.let {
+                      //  mentorList.add(mentor)
+ //   Toast.makeText(this@pg8find, msg, Toast.LENGTH_LONG).show()
+                        if (str == mentor.name){
+                          //  Toast.makeText(this@pg8find, msg, Toast.LENGTH_LONG).show()
+                            msg = "Mentor: ${mentor.name} exists"
+                            Toast.makeText(this@pg8find, msg, Toast.LENGTH_LONG).show()
+                            val intent = Intent(this@pg8find, pg9searchresults::class.java)
+                            intent.putExtra("mid", mentor.mid)
+                            startActivity(intent)
+                            finish()
+                            return
+                        }
+                        else{
+                            msg = "Mentor: ${str} doesn't exists"
+                            Toast.makeText(this@pg8find, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                mentorAdapter.notifyDataSetChanged() // Notify the adapter of changes
+            }
+        })
+
+    }
+    private fun getMentorsList() {
+        databaseReference = FirebaseDatabase.getInstance().getReference("mentors")
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@pg8find, "Database error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mentorList.clear()
+                for (dataSnapshot in snapshot.children) {
+                    val mentor = dataSnapshot.getValue(MentorModel::class.java)
+                    mentor?.let {
+                        mentorList.add(mentor)
+                        val msg = "MID: ${mentor.mid}, Name: ${mentor.name}"
+                        //   Toast.makeText(this@pg7home, msg, Toast.LENGTH_LONG).show()
+                    }
+                }
+                mentorAdapter.notifyDataSetChanged() // Notify the adapter of changes
+            }
+        })
     }
 }
--- 21i0448/app/src/main/java/com/muhammadfakharabbas/i210448/pg9searchresults.kt
+++ 21i0448/app/src/main/java/com/muhammadfakharabbas/i210448/pg9searchresults.kt
@@ -1,13 +1,28 @@
 package com.muhammadfakharabbas.i210448
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class pg9searchresults : AppCompatActivity() {
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var databaseReference: DatabaseReference
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pg9searchresults)
@@ -18,11 +33,68 @@ class pg9searchresults : AppCompatActivity() {
         var add_btn = findViewById<ImageView>(R.id.add_img)
         var chat_btn = findViewById<ImageView>(R.id.chat_img)
         var profile_btn = findViewById<ImageView>(R.id.profile_img)
-        var book1 = findViewById<Button>(R.id.book_btn1)
-        var book2 = findViewById<Button>(R.id.book_btn2)
-        var book3 = findViewById<Button>(R.id.book_btn3)
-        var book4 = findViewById<Button>(R.id.book_btn4)
-        var book5 = findViewById<Button>(R.id.book_btn5)
+
+        val mentorName = findViewById<TextView>(R.id.mentorname)
+        val mentorDesc = findViewById<TextView>(R.id.mentordesc)
+        val mentorStatus = findViewById<TextView>(R.id.mentorstatus)
+        val imgUser = findViewById<ImageView>(R.id.mentorimg)
+        var fee = findViewById<TextView>(R.id.mentorfee)
+
+        var mid = intent.getStringExtra("mid").toString()
+        currentUser = FirebaseAuth.getInstance().currentUser!!
+        databaseReference = FirebaseDatabase.getInstance().getReference("mentors").child(mid)
+
+
+        // Retrieve mentor data from Firebase Realtime Database using mentor ID (mid)
+        databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Check if mentor data exists in the database
+                if (dataSnapshot.exists()) {
+                    // Get mentor data from the snapshot
+                    val mentorNameValue = dataSnapshot.child("name").getValue(String::class.java)
+                    val mentorDescValue = dataSnapshot.child("description").getValue(String::class.java)
+                    val mentorStatusValue = dataSnapshot.child("status").getValue(String::class.java)
+                    val mentorImageUrl = dataSnapshot.child("dpUrl").getValue(String::class.java)
+                    val mentorFeeValue = dataSnapshot.child("fee").getValue(String::class.java)
+
+                    // Set mentor data to the corresponding views
+                    mentorName.text = mentorNameValue
+                    mentorDesc.text = mentorDescValue
+                    mentorStatus.text = mentorStatusValue
+                    fee.text = mentorFeeValue
+
+                    // Load mentor image using Glide or any other image loading library
+                    if (!mentorImageUrl.isNullOrEmpty()) {
+                        Glide.with(this@pg9searchresults)
+                            .load(mentorImageUrl)
+                            .placeholder(R.drawable.profile_image) // Placeholder image while loading
+                            .error(R.drawable.profile_image) // Error image if loading fails
+                            .into(imgUser)
+                    }
+                } else {
+                    // Handle case where mentor data does not exist
+                    Toast.makeText(this@pg9searchresults, "Mentor data not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+                Log.e(TAG, "Error retrieving mentor data: $error")
+                Toast.makeText(this@pg9searchresults, "Error retrieving mentor data", Toast.LENGTH_SHORT).show()
+            }
+        })
+        imgUser.setOnClickListener{
+            Intent(this, pg10mentor::class.java)
+            intent.putExtra("mid",mid)
+            startActivity(intent)
+        }
+        mentorName.setOnClickListener{
+            Intent(this, pg10mentor::class.java)
+            intent.putExtra("mid",mid)
+            startActivity(intent)
+        }
+
+
 
         back_btn.setOnClickListener{
             val intent = Intent(this, pg8find::class.java)
@@ -48,25 +120,6 @@ class pg9searchresults : AppCompatActivity() {
             val intent = Intent(this, pg21profile::class.java)
             startActivity(intent)
         }
-        book1.setOnClickListener{
-            val intent = Intent(this, pg10mentor::class.java)
-            startActivity(intent)
-        }
-        book2.setOnClickListener{
-            val intent = Intent(this, pg10mentor::class.java)
-            startActivity(intent)
-        }
-        book3.setOnClickListener{
-            val intent = Intent(this, pg10mentor::class.java)
-            startActivity(intent)
-        }
-        book4.setOnClickListener{
-            val intent = Intent(this, pg10mentor::class.java)
-            startActivity(intent)
-        }
-        book5.setOnClickListener{
-            val intent = Intent(this, pg10mentor::class.java)
-            startActivity(intent)
-        }
+
     }
 }
